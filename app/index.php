6e79a982fbf170564e882fa6410c92da44e35dd7
Gorazd Krumpak <gorazd@primaprodukcija.si>
2020-04-16 17:39:31 +0200

    Init app

---
+++ app/index.php
@@ -0,0 +1,13 @@
+<?php
+
+  echo (isset($_GET['task']) ? $_GET['task'] : 'task ne obstaja') . '<br>';
+  echo (isset($_GET['param']) ? $_GET['param'] : 'param ne obstaja') . '<br>';
+
+
+
+
+?>
+
+<a href="/udelezenec02/app/">prazno</a><br>
+<a href="/udelezenec02/app/dodaj">dodaj</a><br>
+<a href="/udelezenec02/app/dodaj/123">dodaj/123</a>
