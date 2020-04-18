4113cc8d95786800f6b358f6d9d6d0ab5a8bcb88
Gorazd Krumpak <gorazd@primaprodukcija.si>
2020-04-18 10:52:29 +0200

    Format code

--- api/index.php
+++ api/index.php
@@ -2,33 +2,33 @@
 
   $kupci = array(
     array(
-      'id' => 954,
-      'ime' => 'Miha',
-      'priimek' => 'Kopač',
-      'email' => 'miha.se@gmail.com',
-      'geslo' => sha1('geslo123'),
-      'naslov' => array(
-        'ulica' => 'Slovenska cesta',
-        'hisna_st' => '3',
+      'id'          => 954,
+      'ime'         => 'Miha',
+      'priimek'     => 'Kopač',
+      'email'       => 'miha.se@gmail.com',
+      'geslo'       => sha1( 'geslo123' ),
+      'naslov'      => array(
+        'ulica'     => 'Slovenska cesta',
+        'hisna_st'  => '3',
         'postna_st' => '1000',
-        'posta' => 'Ljubljana'
+        'posta'     => 'Ljubljana'
       ),
-      'kreditna' => 'mastercard',
+      'kreditna'    => 'mastercard',
       'kreditna_st' => '0123 6547 7896'
     ),
     array(
-      'id' => 44,
-      'ime' => 'Metka',
-      'priimek' => 'Novak',
-      'email' => 'metka.novak@gmail.com',
-      'geslo' => sha1('password1'),
-      'naslov' => array(
-        'ulica' => 'Slovenska ulica',
-        'hisna_st' => '13',
+      'id'          => 44,
+      'ime'         => 'Metka',
+      'priimek'     => 'Novak',
+      'email'       => 'metka.novak@gmail.com',
+      'geslo'       => sha1( 'password1' ),
+      'naslov'      => array(
+        'ulica'     => 'Slovenska ulica',
+        'hisna_st'  => '13',
         'postna_st' => '2000',
-        'posta' => 'Maribor'
+        'posta'     => 'Maribor'
       ),
-      'kreditna' => 'Visa',
+      'kreditna'    => 'Visa',
       'kreditna_st' => '0654 7891 3578'
     )
   );
@@ -48,13 +48,13 @@
   } else if ( isset( $_GET['task'] ) && isset( $_GET['username'] ) && isset( $_GET['password'] ) ) {
     //students.b2.eu/undelezenec02/api/prijava/ime@email.com/geslo
 
-    $data = array( 'status' => 'error', 'code' => 404, 'data' => NULL, 'message' => 'Uporabnik ne obstaja' );
+    $data = array( 'status' => 'error', 'code' => 404, 'data' => null, 'message' => 'Uporabnik ne obstaja' );
 
     foreach ( $kupci as $kupec ) {
-      if ( sha1($kupec['email']) === $_GET['username'] && $_GET['password'] === $kupec['geslo']) {
+      if ( sha1( $kupec['email'] ) === $_GET['username'] && $_GET['password'] === $kupec['geslo'] ) {
         $data = array( 'status' => 'success', 'code' => 200, 'data' => $kupec, 'message' => 'uspešno ste se prijavili' );
-      } else if ( sha1($kupec['email']) === $_GET['username'] && $_GET['password'] !== $kupec['geslo']) {
-        $data = array( 'status' => 'error', 'code' => 404, 'data' => NULL, 'message' => 'Geslo ni pravilno' );
+      } else if ( sha1( $kupec['email'] ) === $_GET['username'] && $_GET['password'] !== $kupec['geslo'] ) {
+        $data = array( 'status' => 'error', 'code' => 404, 'data' => null, 'message' => 'Geslo ni pravilno' );
       }
     }
   } else {
@@ -62,8 +62,8 @@
     $data = $kupci;
   }
 
-  header('Content-Type: application/json');
+  header( 'Content-Type: application/json' );
 
-  echo json_encode($data);
+  echo json_encode( $data );
 
 ?>
