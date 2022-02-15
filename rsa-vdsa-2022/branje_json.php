b19160c21b1560a1c751e1cc8629f144083af178
Gorazd Krumpak <gorazd@primaprodukcija.si>
2022-02-15 17:44:03 +0100

    dodaj datum in čas skupaj z lastno funkcijo

--- rsa-vdsa-2022/branje_json.php
+++ rsa-vdsa-2022/branje_json.php
@@ -13,10 +13,67 @@
     <th>Ime</th>
     <th>Priimek</th>
     <th>Naslov</th>
+    <th>Nazadnje prijavljen</th>
+    <th>Registriran ob</th>
+    <th>Trenutni datum in čas</th>
   </tr>
   </thead>
   <tbody>
   <?php
+
+    function pretvoriStevilkoVDanVTednu($dan, $krajsi_format = false, $velika_zacetnika = false) {
+//      if ($dan == 1) {
+//        $danVtednu = 'ponedeljek';
+//      } elseif ($dan == 2) {
+//        $danVtednu = 'torek';
+//      } elseif ($dan == 3) {
+//        $danVtednu = 'sreda';
+//      } elseif ($dan == 4) {
+//        $danVtednu = 'četrtek';
+//      } elseif ($dan == 5) {
+//        $danVtednu = 'petek';
+//      } elseif ($dan == 6) {
+//        $danVtednu = 'sobota';
+//      } else {
+//        $danVtednu = 'nedelja';
+//      }
+
+      switch ($dan) {
+        case 1:
+          $danVtednu = "ponedeljek";
+          break;
+        case 2:
+          $danVtednu = "torek";
+          break;
+        case 3:
+          $danVtednu = "sreda";
+          break;
+        case 4:
+          $danVtednu = "četrtek";
+          break;
+        case 5:
+          $danVtednu = "petek";
+          break;
+        case 6:
+          $danVtednu = "sobota";
+          break;
+        case 7:
+          $danVtednu = "nedelja";
+          break;
+        default:
+          $danVtednu = "";
+      }
+
+      if ($krajsi_format) {
+        $danVtednu = substr($danVtednu, 0, 3);
+      }
+
+      if ($velika_zacetnika) {
+        $danVtednu = ucfirst($danVtednu);
+      }
+
+      return $danVtednu;
+    }
 
     $json = file_get_contents('http://students.b2.eu/udelezenec02/rsa-vdsa-2022/podatki.php');
 
@@ -28,6 +85,10 @@
         echo "<td>" . $oseba->ime . "</td>";
         echo "<td>" . $oseba->priimek . "</td>";
         echo "<td>" . $oseba->naslov->ulica. " " . $oseba->naslov->stevilka . ", " . $oseba->naslov->posta . "</td>";
+        echo "<td>" . $oseba->zadnja_prijava_ob . "</td>"; // YYYY-MM-DD HH:MM:SS
+        echo "<td>" . pretvoriStevilkoVDanVTednu(date("N", strtotime($oseba->registriran_ob)), false, true) . date(", j. n. Y, H:i:s", strtotime($oseba->registriran_ob)) . "</td>"; // YYYY-MM-DD
+      // HH:MM:SS
+        echo "<td>" . pretvoriStevilkoVDanVTednu(date("N"), true, true) . date(", j. n. Y, H:i:s") . "</td>"; // YYYY-MM-DD HH:MM:SS
       echo "</tr>";
     }
   ?>
--- rsa-vdsa-2022/podatki.php
+++ rsa-vdsa-2022/podatki.php
@@ -7,7 +7,9 @@ $podatki = [
       "ulica" => "Slovenska",
       "stevilka" => rand(20,100),
       "posta" => "1000 Ljubljana"
-    ]
+    ],
+    "registriran_ob" => "2021-11-17 17:25:57",
+    "zadnja_prijava_ob" => "2022-01-07 17:25:57"
   ],
   [
     "ime" => "Miša",
@@ -16,7 +18,9 @@ $podatki = [
       "ulica" => "Gorenjska ulica",
       "stevilka" => rand(20,100),
       "posta" => "4000 Kranj"
-    ]
+    ],
+    "registriran_ob" => "2021-11-17 17:25:57",
+    "zadnja_prijava_ob" => "2022-02-14 18:10:19"
   ],
   [
     "ime" => "Peter",
@@ -25,7 +29,9 @@ $podatki = [
       "ulica" => "Primorska ulica",
       "stevilka" => rand(20,100),
       "posta" => "6000 Koper"
-    ]
+    ],
+    "registriran_ob" => "2021-11-17 17:25:57",
+    "zadnja_prijava_ob" => "2022-01-15 17:01:15"
   ],
   [
     "ime" => "Simona",
@@ -34,7 +40,9 @@ $podatki = [
       "ulica" => "Madžarska ulica",
       "stevilka" => rand(20,100),
       "posta" => "7000 Murska Sobota"
-    ]
+    ],
+    "registriran_ob" => "2021-11-17 17:25:57",
+    "zadnja_prijava_ob" => "2022-01-18 19:55:05"
   ]
 ];
 
