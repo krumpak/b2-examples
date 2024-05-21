085c9daa308c634890757da7a1d7966742110fef
Gorazd Krumpak <gorazd@primaprodukcija.si>
2024-05-21 18:33:22 +0200

    seznam

--- vdsa2024/baza.php
+++ vdsa2024/baza.php
@@ -1,3 +1,13 @@
+<!doctype html>
+<html lang="sl">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+
 <?php
 
   include_once 'helper.php';
@@ -8,11 +18,35 @@
   $geslo = getVar('PASSWORD');
 
   try {
-    $conn = new PDO("mysql:host=$gostitelj;dbname=$podatkovna_baza", $uporabnik, $geslo);
+    $conn = new PDO("mysql:host=$gostitelj;dbname=$podatkovna_baza", $uporabnik, $geslo, array( PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
+//    echo "Connected successfully";
   } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
   }
 
+  try {
+    $sql = $conn->prepare("SELECT * FROM zuzelke");
+    $sql->execute();
+    $result = $sql->fetchAll();
+
+    echo "<h1>Seznam žuželk</h1><ul></ul>";
+    foreach ($result as $vrstica) {
+      echo "<li>" . $vrstica['ime'] . "</li>";
+    }
+    echo "</ul>";
+
+  } catch(PDOException $e) {
+    echo "Query failed: " . $e->getMessage();
+  }
+
+
+
+
+
+  $conn = null;
+
 ?>
+
+</body>
+</html>
