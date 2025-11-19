3eefc3205ef5301b099ef61f3b03455b92244ebf
Gorazd Krumpak <gorazd@primaprodukcija.si>
2025-11-19 19:31:28 +0100

    pozdravljen svet

---
+++ rsa-spa-2025/pozdravljen-svet.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Moja prva stran</title>
+        <link href="./slogi.css" rel="stylesheet">
+    </head>
+    <body>
+        <h1>Pozdravljen svet!</h1>
+        <p><strong>Lorem Ipsum</strong> is <u>simply dummy</u> text of the <i>printing and typesetting</i> industry.<hr>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        <a href="https://google.com" target="_blank">Gumb</a>
+        <ol>
+            <li>Zevs</li>
+            <li>Hera</li>
+            <li>Pozejdon</li>
+            <li>Atena</li>
+        </ol>
+        <img src="https://miro.medium.com/v2/resize:fit:1400/1*H90ZEKu0LMMIp96R6jbH3w.png">
+
+        <h1>Odpravljam se na pot!</h1>
+
+        <h1>Moja pot se je končala!</h1>
+    </body>
+</html>
