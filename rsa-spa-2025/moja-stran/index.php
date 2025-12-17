f8e4feffb2b775b3bac01cbf3773f44e5fb947b1
Gorazd Krumpak <gorazd@primaprodukcija.si>
2025-12-17 18:29:30 +0100

    moja stran

---
+++ rsa-spa-2025/moja-stran/index.php
@@ -0,0 +1,130 @@
+<?php
+
+$naslov = "Življenje v naravi";
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php echo $naslov; ?></title>
+    <link rel="stylesheet" href="slogi.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+</head>
+<body>
+
+    <div class="okvir">
+        <header>
+            <img src="./slike/hp-white.svg" alt="Logo">
+            <h1><?php echo $naslov; ?></h1>
+        </header>
+        <nav>
+            <ul>
+                <li><a href="./">Domov</a></li>
+                <li><a class="active" href="./vsebina.html">Vsebina</a></li>
+                <li><a href="./kontakt.html">Kontakt</a></li>
+                <li><a href="./literatura.html">Literatura</a></li>
+            </ul>
+        </nav>
+        <div class="vsebina">
+            <main>
+
+                <article>
+                    <figure>
+                        <img src="./slike/article.png" alt="Slika prvega članka">
+                        <figcaption>Podnapis k moji prvi sliki prvega članka</figcaption>
+                    </figure>
+                    <section>
+                        <h2>Moj prvi članek</h2>
+                        <time datetime="2025-12-03">3. december 2025</time>
+                        <p>Kratek povzetek mojega prvega članka na spletni strani</p>
+                        <a href="">Preberi več</a>
+                    </section>
+                </article>
+
+                <article>
+                    <figure>
+                        <img src="./slike/article.png" alt="Slika prvega članka">
+                        <figcaption>Podnapis k moji prvi sliki prvega članka</figcaption>
+                    </figure>
+                    <section>
+                        <h2>Moj prvi članek</h2>
+                        <time datetime="2025-12-03">3. december 2025</time>
+                        <p>Kratek povzetek mojega prvega članka na spletni strani</p>
+                        <a href="">Preberi več</a>
+                    </section>
+                </article>
+
+                <article>
+                    <figure>
+                        <img src="./slike/article.png" alt="Slika prvega članka">
+                        <figcaption>Podnapis k moji prvi sliki prvega članka</figcaption>
+                    </figure>
+                    <section>
+                        <h2>Moj prvi članek</h2>
+                        <time datetime="2025-12-03">3. december 2025</time>
+                        <p>Kratek povzetek mojega prvega članka na spletni strani</p>
+                        <a href="">Preberi več</a>
+                    </section>
+                </article>
+
+                <article>
+                    <figure>
+                        <img src="./slike/article.png" alt="Slika prvega članka">
+                        <figcaption>Podnapis k moji prvi sliki prvega članka</figcaption>
+                    </figure>
+                    <section>
+                        <h2>Moj prvi članek</h2>
+                        <time datetime="2025-12-03">3. december 2025</time>
+                        <p>Kratek povzetek mojega prvega članka</p>
+                        <a href="">Preberi več</a>
+                    </section>
+                </article>
+
+                <article>
+                    <figure>
+                        <img src="./slike/article.png" alt="Slika prvega članka">
+                        <figcaption>Podnapis k moji prvi sliki prvega članka</figcaption>
+                    </figure>
+                    <section>
+                        <h2>Moj prvi članek</h2>
+                        <time datetime="2025-12-03">3. december 2025</time>
+                        <p>Kratek povzetek mojega prvega članka</p>
+                        <a href="">Preberi več</a>
+                    </section>
+                </article>
+                
+            </main>
+            <aside>
+                <ul>
+                    <li><a href="https://facebook.com" target="_blank">
+                        <img src="./slike/facebook.png" alt="Facebook">
+                        <b>Facebook</b>
+                    </a></li>
+                    <li><a href="https://x.com" target="_blank">
+                        <img src="./slike/x.png" alt="X">
+                        <b>X</b>
+                    </a></li>
+                    <li><a href="https://instagram.com" target="_blank">
+                        <img src="./slike/instagram.webp" alt="Instagram">
+                        <b>Instagram</b>
+                    </a></li> 
+                    <li><a href="https://youtube.com" target="_blank">
+                        <img src="./slike/youtube.webp" alt="YouTube">
+                        <b>YouTube</b>
+                    </a></li>
+                </ul>
+            </aside>
+        </div>
+        <footer>
+            &#169;
+            2025
+            <a href="./">www.moja-stran.si</a>
+        </footer>
+    </div>
+
+</body>
+</html>
