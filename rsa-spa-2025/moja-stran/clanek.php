1fc173ea9c4ab83e667acbd256766e3831cf3ced
Gorazd Krumpak <gorazd@primaprodukcija.si>
2026-01-07 18:35:14 +0100

    clanek

---
+++ rsa-spa-2025/moja-stran/clanek.php
@@ -0,0 +1,27 @@
+<?php
+
+if ( ! defined( 'varovalka' ) ) {
+  exit( '403' );
+}
+
+$clanek = null;
+
+foreach ( $vsebina as $vrstica ) {
+  if ( $vrstica['id'] == $id ) {
+    $clanek = $vrstica;
+    break;
+  }
+} 
+
+?>
+
+<figure>
+    <img src="./slike/<?php echo $clanek['slika']; ?>" alt="<?php echo $clanek['naslov']; ?>">
+    <figcaption><?php echo $clanek['naslov']; ?></figcaption>
+</figure>
+<section>
+    <h2><?php echo $clanek['naslov']; ?></h2>
+    <time datetime="2025-12-03"><?php echo $clanek['datum']; ?></time>
+    <p><?php echo $clanek['clanek']; ?></p>
+    <a href="./index.php">Nazaj</a>
+</section>
